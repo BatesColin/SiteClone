bf6ad0d260f803fb58e6bb3054f7eb58204bf1ca
cb <dragonflylord@gmail.com>
2018-09-05 12:11:20 -0700

    It will now have firebase data in the .env and module.ts

---
+++ src/environments/environment.ts
@@ -0,0 +1,16 @@
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `.angular-cli.json`.
+
+export const environment = {
+  production: false,
+    firebaseConfig : {
+      apiKey: 'AIzaSyCIjcuNcSsGYwNlD7fCooM3AO4WCCFWT58',
+      authDomain: 'siteclone-f73f6.firebaseapp.com',
+      databaseURL: 'https://siteclone-f73f6.firebaseio.com',
+      projectId: 'siteclone-f73f6',
+      storageBucket: 'siteclone-f73f6.appspot.com',
+      messagingSenderId: '1058323291202'
+    }
+};
